ee81bc742d2bd3aa194c6574028a2de4748f8615
fermin234 <fermin234@hotmail.com>
2023-02-18 20:19:57 -0300

    added create & delete

--- server/src/routes/characters.ts
+++ server/src/routes/characters.ts
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { allCharacters, idCharacter } from "../controllers/characters";
+import {
+  allCharacters,
+  idCharacter,
+  createCharacter,
+  deleteCharacters,
+} from "../controllers/characters";
 const router = Router();
 
 router.get("/", allCharacters);
 router.get("/:id", idCharacter);
+router.post("/", createCharacter);
+router.delete("/", deleteCharacters);
 
 export default router;
