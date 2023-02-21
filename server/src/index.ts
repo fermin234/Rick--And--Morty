63312f48f4be8751ffb587c0c216069700efb685
fermin234 <fermin234@hotmail.com>
2023-02-21 16:09:49 -0300

    ruta automatica

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
 import app from "./app";
 import config from "./config/config";
+import axios from "axios";
 import "./db";
 
-app.listen(config.PORT, () => {
+axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
+
+app.listen(config.PORT, async () => {
+  await axios.get(`/character`);
   console.log(`Server listen in port ${config.PORT}`);
 });
