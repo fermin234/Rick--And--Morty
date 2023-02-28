afaac6208af16da4520bc53c61460fee842899b9
fermin234 <fermin234@hotmail.com>
2023-02-27 23:01:24 -0300

    added infinite scroll

--- app/src/redux/actions.js
+++ app/src/redux/actions.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 export const GET_CHARACTERS = "GET_CHARACTERS";
+export const INFINITE_SCROLL = "INFINITE_SCROLL";
 
 export function characters() {
   try {
@@ -8,6 +9,19 @@ export function characters() {
       return dispatch({
         type: GET_CHARACTERS,
         payload: result.data,
+      });
+    };
+  } catch (err) {
+    return err;
+  }
+}
+
+export function fetchMoreData(data) {
+  try {
+    return async (dispatch) => {
+      return dispatch({
+        type: INFINITE_SCROLL,
+        payload: data,
       });
     };
   } catch (err) {
