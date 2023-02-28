ecd221c55a521c182898e4a751286c4e8b46cc44
fermin234 <fermin234@hotmail.com>
2023-02-28 20:58:16 -0300

    changes card

--- app/src/components/Card/Card.jsx
+++ app/src/components/Card/Card.jsx
@@ -1,9 +1,29 @@
 import s from './card.module.css'
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
+//Agregar el punto rojo,verde o gris en caso del status
 
-export default function Card({ id, name, image }) {
+export default function Card({ id, name, image, species, origin, status }) {
   return (
-    <div className={s.contarinerCard} key={id + "*"}>
-      <img className={s.img} src={image} alt={name} />
-    </div>
+    <div className={s.card}>
+      <div className={s.blob}></div>
+      <span className={s.img}><img className={s.img} src={image} alt={id} /></span>
+      <h2>{name}</h2>
+      <h2><span>Especies: {species}</span><br /></h2>
+      <h2><span>Origen: {origin}</span></h2>
+    </div >
   )
 }
