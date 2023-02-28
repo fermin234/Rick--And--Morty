9e384fdeae6dcf077cbc5214407610b25e3d53db
fermin234 <fermin234@hotmail.com>
2023-02-27 23:01:41 -0300

    added infinite scroll

--- app/src/redux/reducer.js
+++ app/src/redux/reducer.js
@@ -1,7 +1,8 @@
-import { GET_CHARACTERS } from "./actions";
+import { GET_CHARACTERS, INFINITE_SCROLL } from "./actions";
 
 const initialState = {
   characters: [],
+  items: [],
 };
 
 export default function rootReducer(state = initialState, action) {
@@ -10,6 +11,18 @@ export default function rootReducer(state = initialState, action) {
       return {
         ...state,
         characters: action.payload,
+        items: [...action.payload.slice(0, 25)],
+      };
+
+    case INFINITE_SCROLL:
+      const sliceCharacters = [
+        ...state.items,
+        ...state.characters.slice(state.items.length, state.items.length + 25),
+      ];
+
+      return {
+        ...state,
+        items: sliceCharacters,
       };
 
     default:
