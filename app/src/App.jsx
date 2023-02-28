51a9d02270e7ca150f2d118c2875c8499bed714c
fermin234 <fermin234@hotmail.com>
2023-02-27 22:58:24 -0300

    refactor app

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,13 +2,29 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/home/Home";
 import landingPage from "./components/LandingPage/LandingPage";
 import './App.css'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { characters } from "./redux/actions";
 
 function App() {
+  const dispactch = useDispatch()
+  const allCharacter = useSelector(s => s.characters)
+
+  useEffect(() => {
+    //-----Si no hay personajes los pido-----
+    if (!allCharacter.length) {
+      console.log("Se piden todos los personajes desde el app.jsx");
+      dispactch(characters())
+    }
+  }, [])
+
+
   return (
     <Router>
       <Switch>
         <Route exact path="/landingPage" component={landingPage} />
-        <Route path="/home" component={Home} />
+        {/* <Route path="/" component={Home} /> */}
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
