1ed5be46f0af3da24268bd0ba51ad632cc915f0d
fermin234 <fermin234@hotmail.com>
2023-03-01 22:05:13 -0300

    changes styles card

--- app/src/components/Card/Card.jsx
+++ app/src/components/Card/Card.jsx
@@ -1,29 +1,17 @@
+import { Box, Img, Radio } from '@chakra-ui/react'
 import s from './card.module.css'
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
-//Agregar el punto rojo,verde o gris en caso del status
 
-export default function Card({ id, name, image, species, origin, status }) {
+export default function Card({ id, name, image, species, origin, status, gender }) {
   return (
     <div className={s.card}>
       <div className={s.blob}></div>
       <span className={s.img}><img className={s.img} src={image} alt={id} /></span>
-      <h2>{name}</h2>
-      <h2><span>Especies: {species}</span><br /></h2>
-      <h2><span>Origen: {origin}</span></h2>
-    </div >
+      <div>
+        <h2>{name}</h2>
+        <h2><span>Especies: {species}</span><br /></h2>
+        <Radio position="absolute" top="2" right="2" size="sm" bgColor={status === "Alive" ? "green" : status === "Dead" ? "red" : "grey"} border="none"></Radio>
+      </div>
+      <Img src={`/src/assets/icons_genres/${gender}.png`} alt="asd" width='5' className={s.img2} margin="2" />
+    </div>
   )
 }
