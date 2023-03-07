992ae22b195d921d6d8b5e5d807da849e1d00fe7
fermin234 <fermin234@hotmail.com>
2023-03-06 22:30:15 -0300

    maquetado pagina

--- app/src/components/Home/Home.jsx
+++ app/src/components/Home/Home.jsx
@@ -2,32 +2,37 @@ import React from "react";
 import NavBar from "../NavBar/NavBar.jsx";
 import ScrollInfinite from "../InfiniteScroll/InfiniteScroll.jsx";
 import Filter from "../Filter/Filter.jsx";
-import { Button, DrawerBody, useDisclosure, Drawer, DrawerOverlay, DrawerContent, Box, Img, background, Tooltip } from '@chakra-ui/react'
+import { Button, DrawerBody, useDisclosure, Drawer, DrawerOverlay, DrawerContent, Box, Img, Tooltip } from '@chakra-ui/react'
 import { CloseIcon } from '@chakra-ui/icons'
 import Banner from "../Banner/Banner.jsx";
 import s from './home.module.css'
+import Statistics from "../Statistics/Statistics.jsx";
 
 export default function Home() {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" overflow="hidden">
+    <Box display="flex" flexDirection="column" overflow="clip" position="relative">
       <NavBar onOpen={onOpen} />
 
-      <Box position="relative" my="4" mx="6%" display="flex" flexDirection="column" alignItems="center" zIndex="1">
-        {/* Banner */}
-        <Banner />
-        {/* ************ */}
+      <Box display="flex" width="100%">
+        <Box className={s.statistics} height="98vh" display="flex" alignItems="end" justifyContent="center" position="sticky" top="0" mx="2">
+          <Statistics position="absolute" />
+        </Box>
+        <Box position="relative" my="4" mr="6%" mt="4%" display="flex" flexDirection="column" alignItems="center" zIndex="1" width="100%">
+          {/* Banner */}
+          <Banner />
+          {/* ************ */}
 
-        {/* ScrollInfinite */}
-        <ScrollInfinite />
-        {/* ************ */}
+          {/* ScrollInfinite */}
+          <ScrollInfinite />
+          {/* ************ */}
 
+        </Box>
       </Box>
 
       {/* Filtro */}
       <Drawer placement='left' onClose={onClose} isOpen={isOpen} size={window.innerWidth > 991 ? "xs" : "r"}>
-        <DrawerOverlay />
         <DrawerContent>
           <Box style={{ display: "flex", justifyContent: "center", position: "relative", width: "100%", height: "50px", alignItems: "center" }}>
             Filters
@@ -50,7 +55,7 @@ export default function Home() {
       </Box>
       {/* ************ */}
 
-    </Box>
+    </Box >
   )
 }
 
