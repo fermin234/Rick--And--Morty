c1b28db60eddb6528a9428358777c45b9e6e77d1
fermin234 <fermin234@hotmail.com>
2023-03-09 09:28:30 -0300

    added config

--- server/src/db.ts
+++ server/src/db.ts
@@ -4,7 +4,8 @@ import config from "./config/config";
 (async () => {
   mongoose.set("strictQuery", false);
   const db = await mongoose.connect(
-    `mongodb://${config.DB_HOST}/${config.DB_NAME}`
+    // `mongodb://${config.DB_HOST}/${config.DB_NAME}`
+    "mongodb://mongo:pmyFjRpwHTlUQZVAw0gQ@containers-us-west-59.railway.app:6763"
   );
   console.log(`Connection stablished to DB: ${db.connection.name}`);
 })();
