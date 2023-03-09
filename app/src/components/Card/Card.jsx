611cfc608b79241295e793285c137f1135752ef7
fermin234 <fermin234@hotmail.com>
2023-03-09 12:59:29 -0300

    fixed gender

--- app/src/components/Card/Card.jsx
+++ app/src/components/Card/Card.jsx
@@ -1,7 +1,7 @@
 import { Box, Img, Radio } from '@chakra-ui/react'
 import s from './card.module.css'
 
-export default function Card({ id, name, image, species, origin, status, gender }) {
+export default function Card({ id, name, image, species, status, gender }) {
   return (
     <div className={s.card}>
       <div className={s.blob}></div>
@@ -11,7 +11,7 @@ export default function Card({ id, name, image, species, origin, status, gender 
         <h2><span>Especies: {species}</span><br /></h2>
         <Radio position="absolute" top="2" right="2" size="sm" bgColor={status === "Alive" ? "green" : status === "Dead" ? "red" : "grey"} border="none"></Radio>
       </div>
-      <Img src={`/src/assets/icons_genres/${gender}.png`} alt="asd" width='5' className={s.img2} margin="2" />
+      <Img src={`/src/assets/icons_genres/${gender.toLowerCase()}.png`} alt="gender" width='5' className={s.img2} margin="2" />
     </div>
   )
 }
