1f00d6056a687fe2359a11ac8c45ac1d3e691c6b
fermin234 <fermin234@hotmail.com>
2023-03-08 22:25:05 -0300

    added dark mode

--- app/src/components/Home/Home.jsx
+++ app/src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import NavBar from "../NavBar/NavBar.jsx";
 import ScrollInfinite from "../InfiniteScroll/InfiniteScroll.jsx";
 import Filter from "../Filter/Filter.jsx";
@@ -8,8 +8,12 @@ import Banner from "../Banner/Banner.jsx";
 import s from './home.module.css'
 import Statistics from "../Statistics/Statistics.jsx";
 import { useSelector } from "react-redux";
+import { GiMoon } from "react-icons/gi";
+import { BsFillMoonFill, BsFillSunFill, BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+  const [fondo, setFondo] = useState(false);
   const { isOpen, onOpen, onClose } = useDisclosure()
   const filterValues = useSelector(s => s.filterValues)
   let [values, setValues] = useState({
@@ -20,14 +24,20 @@ export default function Home() {
     gender: filterValues.gender ? filterValues.gender : null,
   })
 
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
 
   return (
-    <Box display="flex" flexDirection="column" overflow="clip" position="relative">
-      {/* <NavBar onOpen={onOpen} update={update} /> */}
+    <Box className={darkMode ? s.dark : s.light} display="flex" flexDirection="column" overflow="clip" position="relative">
       <NavBar onOpen={onOpen} values={values} setValues={setValues} />
+      {/* darkmode */}
+      <Button position="fixed" left="1" top="100px" zIndex="999" onClick={handleDarkModeToggle} fontSize="100" p="1" bgColor="transparent" border="solid 3px" borderRadius="50%" height="50px" width="50px" color={darkMode ? "white" : "black"}> {darkMode ? <BsFillSunFill /> : <BsFillMoonFill />} </Button>
+      {/* fondo mode */}
+      <Button position="fixed" left="1" top="150px" zIndex="999" fontSize="100" p="1" bgColor="transparent" border="solid 3px" borderRadius="50%" height="50px" width="50px" color={darkMode ? "white" : "black"} onClick={() => setFondo(!fondo)}> {fondo ? <BsFillEyeSlashFill /> : <BsFillEyeFill />} </Button>
 
       {/* Statistics */}
-      <Box display="flex" width="100%">
+      <Box display={fondo ? "none" : "flex"} width="100%">
         <Box className={s.statistics} height="98vh" display="flex" alignItems="end" justifyContent="center" position="sticky" top="0" mx="2">
           <Statistics position="absolute" />
         </Box>
@@ -58,7 +68,6 @@ export default function Home() {
             </Button>
           </Box>
           <DrawerBody>
-            {/* <Filter onClose={onClose} update={update} setUpdate={setUpdate} /> */}
             <Filter onClose={onClose} values={values} setValues={setValues} />
           </DrawerBody>
         </DrawerContent>
