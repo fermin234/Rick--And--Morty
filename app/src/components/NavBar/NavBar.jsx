d25866c7803552676cf96cfc94495f6791cd6e7a
fermin234 <fermin234@hotmail.com>
2023-03-10 20:19:02 -0300

    changes styles

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,16 @@
 import s from './navbar.module.css'
-import { Link, useHistory } from "react-router-dom";
+import { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from "react-redux";
+import { Link, useHistory } from "react-router-dom";
+import { filter } from '../../redux/actions';
 import { Box, Button, FormControl, Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { ArrowRightIcon, Search2Icon } from '@chakra-ui/icons'
-import { useCallback, useState } from 'react';
-import { filter } from '../../redux/actions';
+import { AiFillHome } from "react-icons/ai";
 import debounce from 'lodash.debounce'
+import { IoMdSettings } from "react-icons/io";
 
-export default function NavBar({ onOpen, values, setValues }) {
+
+export default function NavBar({ onOpen, values, setValues, onOpenSettings }) {
   const dispatch = useDispatch()
   const history = useHistory()
   let filterValues = useSelector(s => s.filterValues)
@@ -34,50 +37,45 @@ export default function NavBar({ onOpen, values, setValues }) {
     , []);
 
   return (
-    <Box className={s.containerAll}>
-      {
-        history.location.pathname === "/home"
-          ?
-          <>
-            {/* slideBar */}
-            <Box w="33.3%">
-              <Button colorScheme='blue' onClick={onOpen}>
-                <ArrowRightIcon />
-              </Button>
-            </Box>
-            {/* ********* */}
-
-            {/* input de busqueda */}
-            <Box w="33.3%">
-              <FormControl>
-                <InputGroup position="relative" display="flex" justifyContent="center">
-                  <InputLeftElement
-                    left="20%"
-                    position="absolute"
-                    pointerEvents='none'
-                    children={<Search2Icon color='gray.300' />}
-                  />
-                  <Input id="input" width="60%" placeholder='Rick | Morty | Sanchez' textAlign="center" onChange={debouncedChangeHandler} />
-                  <Button h="80%" zIndex="2" bgColor="transparent" _hover={{ background: "transparent" }} position="absolute" top="1" right="20%" display={inputValue ? "flex" : "none"} onClick={onHandleClick}> X </Button>
-                </InputGroup>
-              </FormControl>
-            </Box>
-            {/* ********* */}
-          </>
-          : ""
-      }
+    <Box className={s.containerAll} >
 
 
-      <Box display="flex" justifyContent="end" gap="10px" w="33.3%">
-        <Link to="/">
-          LandingPage
+
+      {/* slideBar */}
+      <Box visibility={history.location.pathname === "/home" ? "" : "hidden"}>
+        <Button colorScheme='blue' onClick={onOpen}>
+          <ArrowRightIcon />
+        </Button>
+      </Box>
+      {/* ********* */}
+
+      {/* input de busqueda */}
+      <Box w="33%" visibility={history.location.pathname === "/home" ? "" : "hidden"}>
+        <FormControl>
+          <InputGroup position="relative" display="flex" justifyContent="center">
+            <InputLeftElement
+              left="20%"
+              position="absolute"
+              pointerEvents='none'
+              children={<Search2Icon color='gray.300' />}
+            />
+            <Input id="input" width="60%" placeholder='Rick | Morty | Sanchez' textAlign="center" onChange={debouncedChangeHandler} />
+            <Button h="80%" zIndex="2" bgColor="transparent" _hover={{ background: "transparent" }} position="absolute" top="1" right="20%" display={inputValue ? "flex" : "none"} onClick={onHandleClick}> X </Button>
+          </InputGroup>
+        </FormControl>
+      </Box>
+      {/* ********* */}
+
+
+      <Box display="flex" justifyContent="flex-end" gap="10px" fontSize="5vh">
+        <Link to="/" >
+          <AiFillHome />
         </Link>
-        <Link to="/create">
-          Create
-        </Link>
-        <Link to="/home">
-          Home
-        </Link>
+        {/* <Link > */}
+        <Box onClick={() => onOpenSettings()}>
+          <IoMdSettings />
+        </Box>
+        {/* </Link> */}
       </Box>
     </Box >
   )
--- app/src/components/Statistics/Statistics.jsx
+++ app/src/components/Statistics/Statistics.jsx
@@ -1,10 +1,10 @@
 import { Box, Heading } from "@chakra-ui/react";
 import { useSelector } from "react-redux";
 
-export default function Statistics({ fondo }) {
+export default function Statistics() {
   const character = useSelector(s => s.filteredItems)
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" gap="20px" justifyContent="center" bgColor="#4130C2" borderRadius="20px" border=" 3px solid black" transition="all 5s">
+    <Box display="flex" flexDirection="column" alignItems="center" gap="20px" justifyContent="center" bgColor="#4130C2" borderRadius="20px" border=" 3px solid black" transition="all 5s" mx="2">
       {/* Characters */}
       <Box bgColor="#2CB5A0" display="flex" flexDirection="column" alignItems="center" px="1" borderTopRadius="15px">
         <Heading as='h4' size='md'>
@@ -16,53 +16,53 @@ export default function Statistics({ fondo }) {
       </Box>
 
       {/* Status */}
-      <Box bgColor="#2CB5A0" display="flex" flexDirection="column" alignItems="center" >
+      <Box bgColor="#2CB5A0" display="flex" flexDirection="column" alignItems="center" width="100%">
         <Heading as='h4' size='md'>
           Status
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Alive: "}
           {character.filter(e => e.status === "Alive").length}
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Dead: "}
           {character.filter(e => e.status === "Dead").length}
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Unknown: "}
           {character.filter(e => e.status === "unknown").length}
         </Heading>
       </Box>
 
       {/* Gender */}
-      <Box bgColor="#2CB5A0" display="flex" flexDirection="column" alignItems="center" >
+      <Box bgColor="#2CB5A0" display="flex" flexDirection="column" alignItems="center" width="100%">
         <Heading as='h4' size='md'>
           Gender
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Female: "}
           {character.filter(e => e.gender === "Female").length}
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Male: "}
           {character.filter(e => e.gender === "Male").length}
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Genderless: "}
           {character.filter(e => e.gender === "Genderless").length}
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {"Unknown: "}
           {character.filter(e => e.gender === "unknown").length}
         </Heading>
       </Box>
 
       {/* Created */}
-      <Box bgColor="#2CB5A0" w="100%" display="flex" flexDirection="column" alignItems="center" px="1" pt="1" borderBottomRadius="15px">
+      <Box bgColor="#2CB5A0" w="100%" display="flex" flexDirection="column" alignItems="center" px="1" pt="1" borderBottomRadius="15px" width="100%">
         <Heading as='h4' size='md'>
           Created
         </Heading>
-        <Heading as='h4' size='sm'>
+        <Heading as='h4' size='xs'>
           {/* {character.length} */}
           0
         </Heading>
