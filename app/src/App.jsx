143c87966a5bd79f38eb20d3e1f9212ba98b3afc
fermin234 <fermin234@hotmail.com>
2023-03-10 20:17:50 -0300

    fixed fetch data

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,29 +1,9 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import landingPage from "./components/LandingPage/LandingPage";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { characters, getSpecies } from "./redux/actions";
 import FormCreate from "./components/FormCreate/FormCreate";
 
 function App() {
-  const dispactch = useDispatch()
-  const allCharacter = useSelector(s => s.characters)
-  const species = useSelector(s => s.species)
-
-  useEffect(() => {
-    //-----Si no hay personajes, los pido-----
-    if (!allCharacter.length) {
-      dispactch(characters())
-    }
-  }, [])
-
-  useEffect(() => {
-    //-----Si no hay especies, las pido-----
-    if (!species.length) {
-      dispactch(getSpecies())
-    }
-  }, [])
 
   return (
     <Router>
