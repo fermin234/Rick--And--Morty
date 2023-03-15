a7756fbf98df93af8e9b356b3137e14881bef29f
fermin234 <fermin234@hotmail.com>
2023-03-14 22:12:18 -0300

    added settings

--- app/src/components/NavBar/NavBar.jsx
+++ app/src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ import debounce from 'lodash.debounce'
 import { IoMdSettings } from "react-icons/io";
 
 
-export default function NavBar({ onOpen, values, setValues, onOpenSettings }) {
+export default function NavBar({ onOpen, values, setValues, onOpenSettings, setIsOpen2, isOpen2 }) {
   const dispatch = useDispatch()
   const history = useHistory()
   let filterValues = useSelector(s => s.filterValues)
@@ -37,9 +37,7 @@ export default function NavBar({ onOpen, values, setValues, onOpenSettings }) {
     , []);
 
   return (
-    <Box className={s.containerAll} >
-
-
+    <Box className={s.containerAll}>
 
       {/* slideBar */}
       <Box visibility={history.location.pathname === "/home" ? "" : "hidden"}>
@@ -50,29 +48,31 @@ export default function NavBar({ onOpen, values, setValues, onOpenSettings }) {
       {/* ********* */}
 
       {/* input de busqueda */}
-      <Box w="33%" visibility={history.location.pathname === "/home" ? "" : "hidden"}>
+      <Box className={s.containerInput} visibility={history.location.pathname === "/home" ? "" : "hidden"}>
         <FormControl>
           <InputGroup position="relative" display="flex" justifyContent="center">
             <InputLeftElement
-              left="20%"
+              left="0"
               position="absolute"
               pointerEvents='none'
               children={<Search2Icon color='gray.300' />}
             />
-            <Input id="input" width="60%" placeholder='Rick | Morty | Sanchez' textAlign="center" onChange={debouncedChangeHandler} />
-            <Button h="80%" zIndex="2" bgColor="transparent" _hover={{ background: "transparent" }} position="absolute" top="1" right="20%" display={inputValue ? "flex" : "none"} onClick={onHandleClick}> X </Button>
+            <Input id="input" placeholder='Rick | Morty' textAlign="center" onChange={debouncedChangeHandler} _placeholder={{ color: "white" }} />
+            <Button h="80%" zIndex="2" bgColor="transparent" _hover={{ background: "transparent" }} position="absolute" top="1" right="0" display={inputValue ? "flex" : "none"} onClick={onHandleClick}> X </Button>
           </InputGroup>
         </FormControl>
       </Box>
       {/* ********* */}
 
 
-      <Box display="flex" justifyContent="flex-end" gap="10px" fontSize="5vh">
-        <Link to="/" >
-          <AiFillHome />
-        </Link>
+      <Box display="flex" justifyContent="flex-end" gap="10px" fontSize="3vh">
+        <Box visibility={history.location.pathname === "/home" ? "hidden" : ""}>
+          <Link to="/home">
+            <AiFillHome />
+          </Link>
+        </Box>
         {/* <Link > */}
-        <Box onClick={() => onOpenSettings()}>
+        <Box onClick={() => setIsOpen2(!isOpen2)} _hover={{ cursor: "pointer" }}>
           <IoMdSettings />
         </Box>
         {/* </Link> */}
