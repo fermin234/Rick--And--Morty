7a9de7853c1406d051104dbc792a2db06b00c1ac
fermin234 <fermin234@hotmail.com>
2023-03-14 22:11:51 -0300

    fixed image

--- app/src/components/Banner/Banner.jsx
+++ app/src/components/Banner/Banner.jsx
@@ -1,6 +1,9 @@
 import { Box, Checkbox, Heading, Img } from "@chakra-ui/react";
 import { Link, useHistory } from "react-router-dom";
 import s from './banner.module.css'
+import rick_morty from '../../assets/pngCharacter/rick_morty.png'
+import morty from '../../assets/pngCharacter/morty.png'
+import { useState } from "react";
 
 //Agregar mediasquerys por el tema del responsive con la imagen del banner
 //Agregar mediasquerys por el tema del responsive con la imagen del banner
@@ -18,22 +21,33 @@ import s from './banner.module.css'
 //Agregar mediasquerys por el tema del responsive con la imagen del banner
 //Agregar mediasquerys por el tema del responsive con la imagen del banner
 
-export default function Banner() {
+export default function Banner({ setHiddenBanner }) {
   const history = useHistory()
 
+  function handleCheckbox() {
+    localStorage.setItem("hiddenBanner", true)
+    setHiddenBanner(true)
+  }
+
   return (
-    <Box className={s.imagen} bgColor="purple" height={"28vh"} width="100%" borderRadius="30px" p="30px" position="relative" mb="4" overflow="clip">
-      <Heading color="white" fontSize="5xl" ml='10'>Ahora pudes crear tu propio personaje</Heading>
-      <Heading position="absolute" right="40" bottom="2" fontSize="2xl"><Link to="/create">
-        Ir a crear mi personaje aquí
-      </Link></Heading>
+    <Box className={s.imagen} bgColor="purple" width="100%" borderRadius="30px" p="30px" position="relative" mb="4" overflow="clip">
+      <Heading color="white" fontSize="5xl" ml='10'>
+        Ahora pudes crear tu propio personaje
+      </Heading>
+      <Heading fontSize="2xl" ml='10'>
+        <Link to="/create">
+          Ir a crear mi personaje aquí
+        </Link>
+      </Heading>
 
-      <Img src="/src/assets/pngCharacter/rick_morty.png" alt="ryck_morty" position="absolute" bottom='0' right="-150px" width="max-content" zIndex="300" />
+      <Img src={rick_morty} alt="ryck_morty" position="absolute" bottom='0' right="-150px" width="max-content" zIndex="300" />
+      <Img src={morty} alt="morty" position="absolute" bottom='-30px' height="35vh" left="-150px" width="300px" />
 
-      <Img src="/src/assets/pngCharacter/morty.png" alt="morty" position="absolute" bottom='-30px' left="-150px" width="300px" />
       <Box display="flex" position="absolute" bottom="2" ml='30'>
-        <Checkbox />
-        <Heading fontSize="1xl">No volver a mostrar esta informacion.</Heading>
+        <Checkbox onChange={handleCheckbox} />
+        <Heading fontSize="1xl">
+          No volver a mostrar esta informacion.
+        </Heading>
       </Box>
     </Box >
   )
--- app/src/components/Card/Card.jsx
+++ app/src/components/Card/Card.jsx
@@ -1,9 +1,14 @@
 import { Box, Img, Radio } from '@chakra-ui/react'
 import s from './card.module.css'
+import female from "../../assets/icons_genres/female.png"
+import genderless from "../../assets/icons_genres/genderless.png"
+import male from "../../assets/icons_genres/male.png"
+import unknown from "../../assets/icons_genres/unknown.png"
 
 export default function Card({ id, name, image, species, status, gender }) {
+  const dark = JSON.parse(localStorage.getItem("themeDark"))
   return (
-    <div className={s.card}>
+    <Box className={s.card} bgColor={dark ? "#1c3b36" : "#2cb5a0"} color={dark ? "white" : ""} border={dark ? "solid 2px white" : "solid 2px #2cb5a0"}>
       <div className={s.blob}></div>
       <span className={s.img}><img className={s.img} src={image} alt={id} /></span>
       <div>
@@ -11,7 +16,7 @@ export default function Card({ id, name, image, species, status, gender }) {
         <h2><span>Especies: {species}</span><br /></h2>
         <Radio position="absolute" top="2" right="2" size="sm" bgColor={status === "Alive" ? "green" : status === "Dead" ? "red" : "grey"} border="none"></Radio>
       </div>
-      <Img src={`/src/assets/icons_genres/${gender.toLowerCase()}.png`} alt="gender" width='5' className={s.img2} margin="2" />
-    </div>
+      <Img src={gender === "Female" ? female : gender === "Genderless" ? genderless : gender === "Male" ? male : unknown} alt="gender" width='5' className={s.img2} margin="2" />
+    </Box>
   )
 }
