5ca70107897e070c1a4d190d94bb47d18a5ceb1a
fermin234 <fermin234@hotmail.com>
2023-03-20 19:16:43 -0300

    added selects

--- app/src/components/FormCreate/FormCreate.jsx
+++ app/src/components/FormCreate/FormCreate.jsx
@@ -1,24 +1,59 @@
 import { Box, Button, FormControl, Heading, Input, Select, Tag } from "@chakra-ui/react";
+import { useSelector } from "react-redux";
 import NavBar from "../NavBar/NavBar";
 import s from './formCreate.module.css'
 
 export default function FormCreate() {
+  const species = useSelector(s => s.species)
+  const types = useSelector(s => s.types)
+  const origin = useSelector(s => s.origin)
+
   return (
     <Box className={s.containerAll}>
       <NavBar />
       <Box width="100%" height="94vh" display="flex" justifyContent="center" alignItems="center">
         <Box height="90%" w="50%" mt="auto" bgColor="#2cb5a0" borderRadius="35px">
           <FormControl display="flex" height="100%" flexDirection="column" justifyContent="space-evenly" alignItems="center" >
-            <Tag>Name</Tag>
-            <Input name="name" w="75%" />
+            <Box w="100%" position="relative" display="flex" flexDirection="column" alignItems="center">
+              <Tag position="absolute" top="0px" left="0px">Name</Tag>
+              <Input name="name" w="75%" />
+            </Box>
             <Tag>Status</Tag>
-            <Select name="status" w="75%"></Select>
+            <Select name="status" w="75%">
+              <option disabled selected>Selecciona una opción</option>
+              <option value="alive">Alive</option>
+              <option value="dead">Dead</option>
+              <option value="unknown">Unknown</option>
+            </Select>
             <Tag>Gender</Tag>
-            <Select name="gender" w="75%"></Select>
+            <Select name="gender" w="75%">
+              <option disabled selected>Selecciona una opción</option>
+              <option value="female">Demale</option>
+              <option value="male">Male</option>
+              <option value="unknown">Unknown</option>
+              <option value="genderless">Genderless</option>
+            </Select>
             <Tag>Species</Tag>
-            <Select name="species" w="75%"></Select>
+            <Select name="species" w="75%">
+              <option disabled selected>Selecciona una opción</option>
+              {
+                species?.length && species.map(e => <option key={e._id} value={e.name}>{e.name}</option>)
+              }
+            </Select>
             <Tag>Type</Tag>
-            <Select name="type" w="75%"></Select>
+            <Select name="type" w="75%">
+              <option disabled selected>Selecciona una opción</option>
+              {
+                types?.length && types.map(e => <option key={e._id} value={e.name}>{e.name}</option>)
+              }
+            </Select>
+            <Tag>Origin</Tag>
+            <Select name="type" w="75%">
+              <option disabled selected>Selecciona una opción</option>
+              {
+                origin?.length && origin.map(e => <option key={e.name} value={e.name}>{e.name}</option>)
+              }
+            </Select>
             <Button>
               <Heading fontSize="20px">
                 Create character
@@ -36,13 +71,13 @@ export default function FormCreate() {
 //   gender: String;          X
 //   species?: String;          X
 //   type?: String;          X
-//   image?: String;
-//   origin?: {
+//   origin?: {              X
 //     name: String;
 //     url: String;
 //   };
 //   location?: {
-//     name: String;
-//     url: String;
-//   };
-//   episode?: [];
+  //     name: String;
+  //     url: String;
+  //   };
+  //   episode?: [];
+  //   image?: String;
